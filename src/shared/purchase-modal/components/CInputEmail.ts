f6d9e90edd33e6f6091a37bb9f7a50ac809aaeef
Николай <Doonn3@gmail.com>
2022-12-18 19:04:03 +0300

    feat: add unmount and delete comments

--- src/shared/purchase-modal/components/CInputEmail.ts
+++ src/shared/purchase-modal/components/CInputEmail.ts
@@ -20,6 +20,10 @@ class CInputEmail extends BaseComponent {
     }
   }
 
+  public unmount(): void {
+    this.root?.removeEventListener('focusout', this.onInputFocusOut);
+  }
+
   public make(): string {
     const root = `
       <input id="${this.id}"
@@ -29,25 +33,6 @@ class CInputEmail extends BaseComponent {
     `;
     return root.trim();
   }
-
-  // public create(): HTMLInputElement {
-  //   const root: HTMLInputElement = document.createElement('input');
-  //   root.classList.add('input__personal-details');
-  //   this.root = root;
-  //   this.attributes();
-  //   root.addEventListener('focusout', this.onInputFocusOut);
-  //   return root;
-  // }
-
-  // protected attributes(): void {
-  // this.root?.setAttribute('type', 'email');
-  // this.root?.setAttribute('placeholder', this.placeholder);
-  // this.root?.setAttribute('style', this.style());
-  // }
-
-  // protected style(): string {
-  //   return `width: 100%`;
-  // }
 
   private onInputFocusOut = () => {
     if (!(this.root instanceof HTMLInputElement)) return;
