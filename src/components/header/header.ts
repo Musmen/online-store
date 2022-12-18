5d2e68c691e96ca4f3e5a9b78a0955aa9c9d954f
Musmen <musmen@internet.ru>
2022-12-18 23:54:51 +0300

    refactor: change footer, header and not-found-page templates

--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -15,24 +15,7 @@ class HeaderComponent {
   render(): string {
     return `
       <header class="main-header">
-        <h1>Logo from Main Header</h1>
-        <nav class="navigation main-header__navigation">
-          <ul class="list navigation-list main-header__navigation-list">
-            <li class="navigation-item main-header__navigation-item">
-              <a class="navigation-link main-header__navigation-link" href="#/">MainPage</a>
-            </li>
-            <li class="navigation-item main-header__navigation-item">
-              <a class="navigation-link main-header__navigation-link" href="#/cart">CartPage</a>
-            </li>
-            <li class="navigation-item main-header__navigation-item">
-              <a class="navigation-link main-header__navigation-link" href="#/product">ProductPage</a>
-            </li>
-            <li class="navigation-item main-header__navigation-item">
-              <a class="navigation-link main-header__navigation-link" href="#/notfound">404 Page</a>
-            </li>
-          </ul>
-        </nav>
-        <hr>
+        <h1>Main Header. Logo</h1>
       </header>
     `;
   }
