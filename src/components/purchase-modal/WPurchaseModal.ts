b7698ea8c40d9246bc0b836ea98df248665ccc91
Николай <Doonn3@gmail.com>
2022-12-22 16:23:00 +0300

    feat: add submit and validity

--- src/components/purchase-modal/WPurchaseModal.ts
+++ src/components/purchase-modal/WPurchaseModal.ts
@@ -1,3 +1,4 @@
+import BaseComponent from './components/base/BaseComponent';
 import CInputDeliveryAddress from './components/CInputDeliveryAddress';
 import CInputEmail from './components/CInputEmail';
 import CInputPhone from './components/CInputPhone';
@@ -7,12 +8,10 @@ import './style/purchase-modal.scss';
 
 class WPurchaseModal {
   private root: HTMLElement | undefined;
-  private isEnable = false;
-  private isValidate = false;
+  private span: HTMLElement | undefined;
 
   private inputs = {
-    name: new CInputText('Name'),
-    surname: new CInputText('Surname'),
+    name: new CInputText('Name Surname'),
     phone: new CInputPhone('Phone'),
     deliveryAddress: new CInputDeliveryAddress('Delivert address'),
     email: new CInputEmail('E-mail'),
@@ -25,25 +24,29 @@ class WPurchaseModal {
 
   public init(): void {
     this.inputs.name.init();
-    this.inputs.surname.init();
     this.inputs.phone.init();
     this.inputs.deliveryAddress.init();
-    this.inputs.creditCard.init();
     this.inputs.email.init();
+    this.inputs.creditCard.init();
+
     const root: HTMLElement | null = document.querySelector('.purchase-modal');
-    if (root !== null) {
+    const span: HTMLElement | null = document.querySelector('#form-message');
+    if (root !== null && span !== null) {
       root.addEventListener('click', this.onModal);
+      root.querySelector('.submit__purchase-modal')?.addEventListener('click', this.onSubmit);
+      span.classList.add('message--disable');
+      this.span = span;
       this.root = root;
     }
   }
 
   public unmount(): void {
     this.inputs.name.unmount();
-    this.inputs.surname.unmount();
     this.inputs.phone.unmount();
     this.inputs.deliveryAddress.unmount();
-    this.inputs.creditCard.unmount();
     this.inputs.email.unmount();
+    this.inputs.creditCard.unmount();
+
     this.root?.removeEventListener('click', this.onModal);
   }
 
@@ -55,7 +58,6 @@ class WPurchaseModal {
             <div class="personal-details__purchase-modal">
               <span>Personal Detail</span>
               ${this.inputs.name.make()}
-              ${this.inputs.surname.make()}
               ${this.inputs.phone.make()}
               ${this.inputs.deliveryAddress.make()}
               ${this.inputs.email.make()}
@@ -65,7 +67,8 @@ class WPurchaseModal {
               <span>Credit Card Details</span>
               ${this.inputs.creditCard.make()}
             </div>
-            <button class="submit__purchase-modal">CONFIRM</button>
+            <button class="submit__purchase-modal" type="submit">CONFIRM</button>
+            <span id="form-message" class="message__purchase-modal">sad</span>
           <form>
         </div>
       </div>
@@ -76,7 +79,6 @@ class WPurchaseModal {
   private onModal = (event: Event) => {
     if (this.root === undefined) return;
     if (!(event.target instanceof Element)) return;
-
     // if (event.target.closest('.purchase-modal')) {
     if (event.target.className === 'purchase-modal') {
       this.root.style.display = 'none';
@@ -84,6 +86,38 @@ class WPurchaseModal {
       console.log(event.target);
     }
   };
+
+  private onSubmit = () => {
+    if (this.span === undefined) return;
+
+    const vals = Object.values(this.inputs);
+
+    if (this.span.classList.contains('message--disable')) {
+      this.span.classList.remove('message--disable');
+    }
+
+    if (this.isValidate(vals)) {
+      this.span.textContent = 'SUCCESS: Please wait 3 seconds';
+      setTimeout(() => {
+        this.unmount();
+        window.location.replace('?#/');
+      }, 3000);
+    } else {
+      this.span.textContent = 'Invalide';
+    }
+  };
+
+  private isValidate(arr: unknown[]): boolean {
+    const temp = [];
+    for (let i = 0; i < arr.length; i++) {
+      const elem = arr[i];
+      if (elem instanceof BaseComponent || elem instanceof WCreditCard) {
+        temp.push(elem.checkValidity());
+      }
+    }
+
+    return temp.every((a) => a === true);
+  }
 }
 
 export default WPurchaseModal;
