2a409b88d1f937009ec8605bbf51ad18c8296ab0
Николай <Doonn3@gmail.com>
2022-12-22 16:27:48 +0300

    feat: add check validity

--- src/components/purchase-modal/components/CInputEmail.ts
+++ src/components/purchase-modal/components/CInputEmail.ts
@@ -5,9 +5,7 @@ class CInputEmail extends BaseComponent {
   private id = 'input-email';
   private placeholder: string;
 
-  private isValidate = false;
-
-  constructor(placeholder = 'pldaceholder') {
+  constructor(placeholder = 'placeholder') {
     super();
     this.placeholder = placeholder;
   }
@@ -15,32 +13,61 @@ class CInputEmail extends BaseComponent {
   public init(): void {
     const input: HTMLInputElement | null = document.querySelector(`#${this.id}`);
     if (input !== null) {
-      input.addEventListener('focusout', this.onInputFocusOut);
+      input.addEventListener('input', this.onInput);
+      input.addEventListener('focus', this.onInputFocus);
       this.root = input;
     }
   }
 
   public unmount(): void {
-    this.root?.removeEventListener('focusout', this.onInputFocusOut);
+    this.root?.removeEventListener('input', this.onInput);
+    this.root?.removeEventListener('focus', this.onInputFocus);
   }
 
   public make(): string {
     const root = `
       <input id="${this.id}"
             class="input-modal"
-            required type="email"
+            type="text"
             placeholder="${this.placeholder}">
     `;
     return root.trim();
   }
 
-  private onInputFocusOut = () => {
+  private onInput = (event: Event) => {
     if (!(this.root instanceof HTMLInputElement)) return;
-    this.validate(this.root.value);
+    if (!(event instanceof InputEvent)) return;
+
+    if (event.inputType === 'deleteContentBackward') {
+      this.tempValue = this.tempValue.slice(0, -1);
+    } else if (event.data === ' ') {
+      const clear = this.root.value.slice(0, -1);
+      this.root.value = clear;
+    } else {
+      this.tempValue += event.data;
+      this.root.value = this.tempValue;
+    }
   };
 
-  private validate(val: string): void {
-    this.isValidate = val.includes('@');
+  private onInputFocus = () => {
+    if (!(this.root instanceof HTMLInputElement)) return;
+
+    if (this.root.classList.contains('validation-error')) {
+      this.root.classList.remove('validation-error');
+      this.root.value = this.tempValue;
+    }
+  };
+
+  public checkValidity(): boolean {
+    if (!(this.root instanceof HTMLInputElement)) return false;
+    const check = this.root.value.split(/[\\@.]+/).filter((x) => x);
+
+    if (check.length < 3) {
+      this.root.classList.add('validation-error');
+      this.root.value = this.errorText;
+      return false;
+    }
+    return true;
   }
 }
 
