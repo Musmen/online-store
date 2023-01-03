b3daf160aa11fc7ff819de4771e85ba383d5b068
Николай <Doonn3@gmail.com>
2023-01-03 22:47:13 +0300

    feat: add event

--- src/components/product-item/product-item.ts
+++ src/components/product-item/product-item.ts
@@ -1,4 +1,5 @@
 import { convertToRomane } from '../../common/common.helper';
+import EventBus from '../../eventbus/EventBus';
 import { ProductItem } from '../../models/product-item.model';
 
 export default class ProductItemComponent {
@@ -9,17 +10,37 @@ export default class ProductItemComponent {
     this.render = this.render.bind(this);
   }
 
+  private id: string | number = 0;
+  private root: HTMLElement | null = null;
+  private btn: HTMLButtonElement | null = null;
+  private link = '#';
+  private isInCart = false;
+  public init(): void {
+    const root: HTMLElement | null = document.querySelector(`[data-id="${this.id}"]`);
+    if (root === null) return;
+    const btn: HTMLButtonElement | null = root.querySelector('.add-cart-btn');
+    btn?.addEventListener('click', this.onButton);
+    this.btn = btn;
+    this.root = root;
+  }
+
+  public unmount(): void {
+    this.btn?.removeEventListener('click', this.onButton);
+  }
+
   render(): string {
     if (!this.#product) return '';
 
     const { id, name, short_name, price, tier, nation, type, images, amount } = this.#product;
+    this.id = id;
 
     const flagClassName = `flag flag_${nation}`;
     const typeClassName = `tank-type tank-type_${type?.toLowerCase()}`;
 
     const linkToProductPage = `#/product/${id}`;
+    this.link = linkToProductPage;
     const isInCart = Math.random() > 0.5 ? true : false;
-
+    this.isInCart = isInCart;
     return `
       <article class="card" data-id="${id}">
         <a href="${linkToProductPage}" class="card-info">
@@ -40,4 +61,9 @@ export default class ProductItemComponent {
         <button class="add-cart-btn ${isInCart ? 'add-cart-btn_active' : ''}">Cart</button>
       </article>`;
   }
+
+  private onButton = () => {
+    if (this.#product === null) return;
+    EventBus.emit<ProductItem, string>('product', this.#product, this.link);
+  };
 }
