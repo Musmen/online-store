f534c75189704cd4ceac57f2e2e4485a43e5cd6a
Musmen <musmen@internet.ru>
2023-01-05 12:06:39 +0300

    feat: add dual-slider component

---
+++ src/components/main-page/components/dual-slider/common/dual-slider.helper.ts
@@ -0,0 +1,22 @@
+import { DualSliderColors, MinMaxRange } from '../models/dual-slider.model';
+
+type GetSliderBackgroundType = (limits: MinMaxRange, values: MinMaxRange, colors: DualSliderColors) => string;
+
+export const getSliderBackground: GetSliderBackgroundType = (
+  limits: MinMaxRange,
+  values: MinMaxRange,
+  colors: DualSliderColors
+) => {
+  const rangeDistance: number = limits.max - limits.min;
+  const fromPosition: number = values.min - limits.min;
+  const toPosition: number = values.max - limits.min;
+
+  return `linear-gradient(
+    to right,
+    ${colors.slider} 0%,
+    ${colors.slider} ${(fromPosition / rangeDistance) * 100}%,
+    ${colors.fill} ${(fromPosition / rangeDistance) * 100}%,
+    ${colors.fill} ${(toPosition / rangeDistance) * 100}%,
+    ${colors.slider} ${(toPosition / rangeDistance) * 100}%,
+    ${colors.slider} 100%)`;
+};
---
+++ src/components/main-page/components/dual-slider/models/dual-slider.model.ts
@@ -0,0 +1,9 @@
+export interface MinMaxRange {
+  min: number;
+  max: number;
+}
+
+export interface DualSliderColors {
+  slider: string;
+  fill: string;
+}
