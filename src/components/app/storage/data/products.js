b127df318c33a99240b6997f9ad6a71f51cb4d24
Musmen <musmen@internet.ru>
2023-01-06 14:11:26 +0300

    refactor: change products data for better feature testing

--- src/components/app/storage/data/products.js
+++ src/components/app/storage/data/products.js
@@ -3,7 +3,7 @@ export default [
     description:
       'Developed in the summer of 1944 by the Construction Bureau of the Kirov Plant (Leningrad). In the fall of the same year, the prototype underwent trials. It never entered mass production nor saw service.\nThis Premium vehicle has a 10% bonus XP earn and a 50% bonus Silver earn.',
     short_name: 'IS-6',
-    price: 118,
+    price: 61,
     amount: 23,
     nation: 'ussr',
     images: [
@@ -37,7 +37,7 @@ export default [
     description:
       "Development began in June 1944 at the Uralmash Design Bureau. The vehicle was based on the T-44 chassis and had a conventional configuration, with a forward placement of the fighting compartment. In October, the People's Commissariat of the Tank Industry considered the SU-122-44 project. In December, the design project was ready. However, the SU-100M-2 with a rear placement of the fighting compartment was preferred.\nThis Premium vehicle has a 20% bonus XP earn and a 44% bonus Silver earn.",
     short_name: 'SU-122-44',
-    price: 67.5,
+    price: 86.5,
     amount: 7,
     nation: 'ussr',
     images: [
@@ -54,7 +54,7 @@ export default [
     description:
       'Some 400 of these French tanks were captured by the German Army. They were later used for training purposes and on secondary fronts.\nThis Premium vehicle has a 60% bonus XP earn and a 26% bonus Silver earn.',
     short_name: 'Pz. S35',
-    price: 10,
+    price: 21,
     amount: 1,
     nation: 'germany',
     images: [
@@ -72,8 +72,8 @@ export default [
     description:
       'This experimental light tank was developed at the Škoda factory from 1941 through 1943, with a total of five prototypes manufactured. The vehicle was never mass-produced, nor saw service.\nThis Premium vehicle has a 60% bonus XP earn and a 50% bonus Silver earn.',
     short_name: 'Pz. T 15',
-    price: 9,
-    amount: 4,
+    price: 14,
+    amount: 14,
     nation: 'germany',
     images: [
       'https://catoolwebdav-net-cdn.gcdn.co/catool/ada9d5f1d94b7d8db983f33ac3666685.png',
@@ -90,8 +90,8 @@ export default [
     description:
       'Developed together with the T-126 at Factory No. 174 during the summer of 1940. Vehicle development was suspended during the preliminary design phase and then discontinued due to problems with the V-3 diesel engine.\nThis Premium vehicle has a 60% bonus XP earn and a 31% bonus Silver earn.',
     short_name: 'T-127',
-    price: 12,
-    amount: 5,
+    price: 28,
+    amount: 25,
     nation: 'ussr',
     images: [
       'https://catoolwebdav-net-cdn.gcdn.co/catool/3e69a7d9477ff8ff93ddada4fe5f6ad4.png',
@@ -108,7 +108,7 @@ export default [
     description:
       'The Dicker Max ("Fat Max") was initially intended to destroy bunkers. Two prototypes were built in the spring of 1941. The vehicle was later redesignated as a heavy tank destroyer. The design was never put into mass production, but the prototypes fought on the Eastern Front.\nThis Premium vehicle has a 30% bonus XP earn and a 35% bonus Silver earn.',
     short_name: 'D. Max',
-    price: 32,
+    price: 82,
     amount: 34,
     nation: 'germany',
     images: [
@@ -126,8 +126,8 @@ export default [
     description:
       'The vehicle was proposed as a further development of the Ru 251 and primarily intended for export, particularly to Mexico. This variant inherited the same armor and armament from its predecessor, but featured an elongated hull and an additional support roller. The vehicle existed only in blueprints.\nThis vehicle has a 10% XP earn bonus and a 50% Silver earn bonus.',
     short_name: 'Águila',
-    price: 65,
-    amount: 14,
+    price: 15,
+    amount: 48,
     nation: 'germany',
     images: [
       'https://catoolwebdav-net-cdn.gcdn.co/catool/3de4dfd00f90d69e9c83a1e38df45480.png',
@@ -144,8 +144,8 @@ export default [
     description:
       'The first prototype was constructed at the end of 1944. After successful trials in 1945–1947 the T-54 mod. 1947 was adopted for service. A total of 713 vehicles were constructed before production was phased out. In 1949, production of a new variant was launched.',
     short_name: 'T-54',
-    price: 3450,
-    amount: 8,
+    price: 34,
+    amount: 71,
     nation: 'ussr',
     images: [
       'https://catoolwebdav-net-cdn.gcdn.co/catool/631fcf75fb2859ad7447f94904dc4cc9.png',
@@ -180,8 +180,8 @@ export default [
     description:
       'The M2 prototype with Vickers suspension. After trials in 1934, the tank was sent for redesign, as the performance of the suspension was not sufficient. The tank never saw mass production.\nThis Premium vehicle has a 75% bonus XP and a 27% bonus Silver earn.',
     short_name: 'T2 Light',
-    price: 75,
-    amount: 26,
+    price: 7,
+    amount: 12,
     nation: 'usa',
     images: [
       'https://catoolwebdav-net-cdn.gcdn.co/catool/e64d973a2a1ff352952d3d034c491c99.png',
@@ -200,7 +200,7 @@ export default [
       'Initially designed for airborne operations, the vehicle was never used as such by the U.S.A. during World War II. In the absence of a good air delivery system, the Ordnance Department became less enthusiastic about any further development.\nThis Premium vehicle has a 60% bonus XP earn and a 57% bonus Silver earn.',
     short_name: 'Locust',
     price: 9,
-    amount: 17,
+    amount: 47,
     nation: 'usa',
     images: [
       'https://catoolwebdav-net-cdn.gcdn.co/catool/d1e1bc168ce1321f103d69598ff8a932.png',
@@ -237,8 +237,8 @@ export default [
     description:
       "At the end of 1942, the Churchill tank's armament was deemed insufficient. New tanks, the A31, A32, and A33 were developed as new infantry tanks. The A33 was developed by the English Electric Company. The LMS Company participated in the development of suspension for a new tank. Two vehicles, which differed greatly from each other, were manufactured. The A33 was set to go into production but, in 1943, the decision to cancel mass production of the Churchill was reversed, and the order for the A33 was not placed.\nThis Premium vehicle has a 40% bonus XP earn and a 40% bonus Silver earn.",
     short_name: 'Excelsior',
-    price: 15,
-    amount: 2,
+    price: 92,
+    amount: 5,
     nation: 'uk',
     images: [
       'https://catoolwebdav-net-cdn.gcdn.co/catool/573e53a00a46fd099cf2097a95c7537f.png',
@@ -275,8 +275,8 @@ export default [
     description:
       'Developed starting in 1944 by English Electric as a possible replacement for the A43 Black Prince. Some components were unified with the A41 Centurion. In 1948, trials of a prototype were started which mounted a Centurion Mk. II turret and a 17-pounder gun. Due to changes in Armored Forces policy and doctrine, development proceeded no further. However, the design later served as a basis for several post-war heavy tanks.\nThis Premium vehicle has a 20% bonus XP earn and a 30% bonus Silver earn.',
     short_name: 'FV201 (A45)',
-    price: 49.5,
-    amount: 17,
+    price: 59.5,
+    amount: 67,
     nation: 'uk',
     images: [
       'https://catoolwebdav-net-cdn.gcdn.co/catool/66daffac35b3c6708dd7e3f0beeb9ab6.png',
@@ -313,7 +313,7 @@ export default [
     description:
       "A tank under Sergeant Bob Early's command, with serial number 26, used by Company E of the 32nd Armored Regiment of the 3rd Armored Division. This vehicle was one of the pilot T26E3s, which participated in Operation Zebra (to trial the Pershing tanks in Europe). In the battle for Cologne on May 6, 1945, Early's crew destroyed the German Panther in front of the Cologne Cathedral with three shots.\nThis Premium vehicle has a 20% bonus XP earn and a 30% bonus Silver earn.",
     short_name: 'Eagle 7',
-    price: 67,
+    price: 26,
     amount: 10,
     nation: 'usa',
     images: [
@@ -332,8 +332,8 @@ export default [
     description:
       'Experimental tank on the basis of the Centurion medium tank. The vehicle was in development from 1956 through 1959. Never saw mass production. Technical decisions and innovations implemented on the FV4202 became the basis for the FV4201 Chieftain.',
     short_name: 'FV4202',
-    price: 610,
-    amount: 9,
+    price: 31,
+    amount: 59,
     nation: 'uk',
     images: [
       'https://catoolwebdav-net-cdn.gcdn.co/catool/b4f57b756c7d26e8df6c4577cfaa9a5c.png',
@@ -350,8 +350,8 @@ export default [
     description:
       'A project of the Chrysler Corporation, developed in the summer of 1946. The vehicle featured a number of design solutions that were not typical of U.S. tank-building. In particular, the Chrysler K was to have its transmission and engine located in the front, and the armament compartment in the rear of the hull. All crew members would have sat in the turret. It is thought that this project was the first U.S. project to position the driver in the turret. It was not supported by the army due to limited funding.\nThis Premium vehicle has a 10% bonus XP earn and a 50% bonus Silver earn.',
     short_name: 'Chrysler K',
-    price: 92,
-    amount: 11,
+    price: 79,
+    amount: 25,
     nation: 'usa',
     images: [
       'https://catoolwebdav-net-cdn.gcdn.co/catool/709e29bdc1aa93ca2b4a080692967d58.png',
