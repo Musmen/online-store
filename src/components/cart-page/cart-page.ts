14f1fa5c8646a0a9dc82c3eee4ac272089fe54ed
Николай <Doonn3@gmail.com>
2023-01-07 19:40:52 +0300

    refactor: cart-page

--- src/components/cart-page/cart-page.ts
+++ src/components/cart-page/cart-page.ts
@@ -1,11 +1,9 @@
 import './scss/cart.styles.scss';
 import EventBus from '../../eventbus/EventBus'; // << Test Event Bus
-import { ProductItem } from '../../models/product-item.model';
 import TotalController from './widgets/total/controller/TotalController';
-import ProductStoreModel from './models/ProductStoreModel';
+import CartStoreService from './models/CartStoreService';
 
 import ProductCartController from './widgets/product-cart/controller/ProductCartController';
-import CartStorageService from './service/CartStorageService';
 import EventCartEmpty from '../../eventbus/events/EventCartEmpty';
 
 export class CartComponent {
@@ -14,37 +12,17 @@ export class CartComponent {
   private productCartController: ProductCartController = new ProductCartController();
   private totalController: TotalController = new TotalController();
 
-  private productList = ProductStoreModel;
   constructor() {
     this.init = this.init.bind(this);
     this.unmount = this.unmount.bind(this);
     this.render = this.render.bind(this);
-    EventBus.subscribe(this, 'product', this.onProduct);
   }
-
-  private onProduct = (product: ProductItem, link: string | undefined) => {
-    if (product === null) return;
-    if (link !== undefined) {
-      this.productList.add(product, link);
-    }
-  };
 
   private initProductList(): void {
-    for (let i = 0; i < this.productList.Products.length; i++) {
-      const product = this.productList.Products[i];
-      this.productCartController.addProduct(product, this.productList.Links[i]);
+    for (let i = 0; i < CartStoreService.Products.length; i++) {
+      const product = CartStoreService.Products[i];
+      this.productCartController.addProduct(product);
     }
-  }
-
-  // TEST
-  private isEmptyStorage(): void {
-    const test = CartStorageService.getFrom('products');
-    if (test.length <= 0) return;
-    this.productList.reset();
-    for (let i = 0; i < test.length; i++) {
-      this.productList.add(test[i]);
-    }
-    CartStorageService.deleteFrom('products');
   }
 
   init(): void {
@@ -52,7 +30,9 @@ export class CartComponent {
     if (root === null) return;
     this.root = root;
 
-    if (this.productList.Products.length <= 0) {
+    CartStoreService.init();
+
+    if (CartStoreService.Products.length <= 0) {
       this.emptyCart();
     }
 
@@ -93,13 +73,6 @@ export class CartComponent {
         ${this.totalController.render()}
       </section>
     `;
-  }
-
-  public makeEmptyCart(): string {
-    const elem = `
-      <div class="cart-empty"></div>
-    `;
-    return elem.trim();
   }
 }
 
