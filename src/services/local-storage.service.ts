12f3f980fb49598e0e154367e1e9d820879b5be1
Николай <Doonn3@gmail.com>
2023-01-08 19:21:18 +0300

    refactor: changed access to public

--- src/services/local-storage.service.ts
+++ src/services/local-storage.service.ts
@@ -1,15 +1,15 @@
 import { LOCAL_STORAGE_KEYS } from './common/services.constants';
 
 class LocalStorageService {
-  #setLocalStorageData(value: unknown, key: string = LOCAL_STORAGE_KEYS.DEFAULT) {
+  public setLocalStorageData(value: unknown, key: string = LOCAL_STORAGE_KEYS.DEFAULT) {
     localStorage.setItem(key, JSON.stringify(value));
   }
 
-  #getFromLocalStorage(key: string = LOCAL_STORAGE_KEYS.DEFAULT) {
-    return JSON.parse(localStorage.getItem(key) || '');
+  public getFromLocalStorage(key: string = LOCAL_STORAGE_KEYS.DEFAULT) {
+    return JSON.parse(localStorage.getItem(key) || '[]');
   }
 
-  #deleteFromLocalStorage(key: string = LOCAL_STORAGE_KEYS.DEFAULT) {
+  public deleteFromLocalStorage(key: string = LOCAL_STORAGE_KEYS.DEFAULT) {
     localStorage.removeItem(key);
   }
 }
