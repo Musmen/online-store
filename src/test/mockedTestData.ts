a991af8911709978aab71165edbf42cb314cb582
Musmen <musmen@internet.ru>
2023-01-08 10:52:08 +0300

    feat: add tests for helpers functions from 'src/services/common/services.helpers.ts'

--- src/test/mockedTestData.ts
+++ src/test/mockedTestData.ts
@@ -1,5 +1,6 @@
 import { MinMaxRange } from '../models/common.model';
 import { ProductItem } from '../models/product-item.model';
+import { MinMaxTupleType } from '../services/models/services.models';
 
 export const mockedProductsOnlyWithNumericProperties = [
   {
@@ -35,6 +36,14 @@ export const minMaxPropertiesForProductsOnlyWithNumericProperties: {
   price: { min: 26, max: 65 },
   amount: { min: 10, max: 67 },
   tier: { min: 1, max: 10 },
+};
+
+export const minMaxPropertiesTuplesForProductsOnlyWithNumericProperties: {
+  [key: string]: MinMaxTupleType;
+} = {
+  price: [26, 65],
+  amount: [10, 67],
+  tier: [1, 10],
 };
 
 export const mockedProductsWithInvalidData = [
